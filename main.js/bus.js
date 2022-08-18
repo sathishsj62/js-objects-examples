f49cc48b1a3c407807c35f4546724384cf99a6d0
SathishKumar <sathishvivis6620@gmail.com>
2022-08-18 21:23:35 +0530

    modified:index.html

--- main.js/bus.js
+++ main.js/bus.js
@@ -1,8 +1,19 @@
-let myBus= {
-    brand:'tata',
-    nmae :'bus',
-    type :'diseal',
-    color :'red'
+// let myBus= {
+//     brand:'tata',
+//     nmae :'bus',
+//     type :'diseal',
+//     color :'red'
+
+// }
+// console.log(myBus);
+// alert('hii')
+function thayakatta(){
+let x=[1,2,3,4,5,6];
+let y=Math.floor(Math.random()*x.length);
+let z=x[y];
+console.log(z);
+document.getElementById('thayam').innerHTML=z;
 
 }
-console.log(myBus);
+let button=document.getElementById('btn');
+button.addEventListener('click',thayakatta)
